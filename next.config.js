4441c4436ccebc451c04d6e02ad1801c49d1952f
anirudhkhatry <anirudhkhatry@gmail.com>
2025-06-10 00:38:01 +0000

    build bug resolved

--- next.config.js
+++ next.config.js
@@ -2,9 +2,8 @@
 const nextConfig = {
   output: 'export', // This line tells Next.js to export your application as static HTML
   // You can add other Next.js configurations here if needed
-  images: {
-    unoptimized: true,
-  },
+  basePath: '', // For reponame.github.io, use your repo name
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
