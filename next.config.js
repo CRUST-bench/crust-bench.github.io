95d960e40aad6be0df4dab4956ea0915a045ea05
anirudhkhatry <anirudhkhatry@gmail.com>
2025-06-10 00:25:45 +0000

    bug resolved

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
+/* @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // This line tells Next.js to export your application as static HTML
+  // You can add other Next.js configurations here if needed
 };
+
+module.exports = nextConfig;
